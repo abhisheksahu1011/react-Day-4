8c1202d7690d0138ff157d76fa37b07a0eb6a6f6
abhisheksahu1011 <sahuabhishek1011@gmail.com>
2022-09-12 18:31:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import CallBack from './Components/CallBack'
+import Context from './Components/Context'
+import PropsDrelling from './Components/PropsDrelling'
+import UseRef from './Components/UseRef'
+
+const App = () => {
+  return (
+    <>
+      <UseRef/>
+      <Context/>
+      <CallBack/>
+      <PropsDrelling/>
+    </>
+  )
+}
+
+export default App
